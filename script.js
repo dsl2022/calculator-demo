9ab4ac3674d402fbcf8f4b4d757097c10f38036d
dsl2055 <job2019tmm@gmail.com>
2022-11-21 21:34:01 -0500

    second

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let result = 0;
+const numArr = [];
 // HTML
 // container with id
 
@@ -9,3 +11,50 @@
 // functions for all operations.
 // add, sub, mul, div. equal, percentage, negative.
 // ac and c
+
+const display = document.querySelector("#display");
+display.textContent = result;
+display.style.color = "white";
+display.style.fontSize = "4rem";
+
+const add = document.querySelector(".add");
+add.addEventListener("click", () => {
+  numArr.push("+");
+  console.log({ numArr });
+});
+
+const equal = document.querySelector(".equal");
+equal.addEventListener("click", () => {});
+
+const oneKey = document.querySelector(".one");
+oneKey.addEventListener("click", () => {
+  numArr.push(1);
+  console.log({ numArr });
+});
+
+const twoKey = document.querySelector(".two");
+twoKey.addEventListener("click", () => {
+  numArr.push(2);
+  console.log({ numArr });
+});
+
+const threeKey = document.querySelector(".three");
+threeKey.addEventListener("click", () => {
+  console.log("working");
+});
+
+function addition(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function mul(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  a / b;
+}
