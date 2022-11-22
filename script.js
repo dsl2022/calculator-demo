f9dc32eb1515aafd0bad19b25a4af2e10899a3c0
dsl2055 <job2019tmm@gmail.com>
2022-11-21 21:12:16 -0500

    first

---
+++ script.js
@@ -0,0 +1,11 @@
+// HTML
+// container with id
+
+// JS
+// TODO
+// init state
+// number 1 - 9, buttons.AC button, +/-, %, (+ - * / =)
+
+// functions for all operations.
+// add, sub, mul, div. equal, percentage, negative.
+// ac and c
